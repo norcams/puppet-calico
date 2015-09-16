2fd57e338e3e6c061b70c319fdc38056606a7ec0
Jan Ivar Beddari <code@beddari.net>
2015-09-16 16:23:50 +0200

    Managing dhcp agent and nova metadata api

--- manifests/compute.pp
+++ manifests/compute.pp
@@ -3,11 +3,22 @@
 class calico::compute (
 ) inherits calico::params {
 
-  include calico::compute::qemu
+  include ::calico::compute::qemu
+  include ::neutron::agents::dhcp
+
+  package { $compute_metadata_package:
+    ensure => installed,
+  }
+
+  service { $compute_metadata_service:
+    ensure => running,
+  }
 
   package { $compute_package:
     ensure => installed,
   }
+
+  service [
 
   exec { 'cp_default_config':
     path    => ['/usr/bin', '/usr/sbin', '/bin'],
@@ -23,5 +34,8 @@ class calico::compute (
   Package[$compute_package] ->
   Exec['cp_default_config'] ~>
   Service[$compute_service]
+
+  Package[$compute_metadata_package] ~>
+  Service[$compute_metdata_service]
 
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -3,37 +3,43 @@
 # Default parameter values
 #
 class calico::params {
-  $ensure                     = present
-  $manage_packages            = false
-  $manage_repo                = false
-  $manage_epel                = false
-  $compute_package_default    = 'calico-compute'
-  $compute_service_default    = 'calico-felix'
-  $controller_package_default = 'calico-control'
-  $controller_service_default = 'neutron-server'
-  $libvirt_service_default    = 'libvirtd'
-  $qemu_conf_default          = '/etc/libvirt/qemu.conf'
+  $ensure                           = present
+  $manage_packages                  = false
+  $manage_repo                      = false
+  $manage_epel                      = false
+  $compute_metadata_service_default = 'openstack-nova-api'
+  $compute_metadata_package_default = 'openstack-nova-metadata-api'
+  $compute_package_default          = 'calico-compute'
+  $compute_service_default          = 'calico-felix'
+  $controller_package_default       = 'calico-control'
+  $controller_service_default       = 'neutron-server'
+  $libvirt_service_default          = 'libvirtd'
+  $qemu_conf_default                = '/etc/libvirt/qemu.conf'
   case $::osfamily {
     'Debian' : {
       case $::operatingsystem {
         default: {
-          $compute_package    = $compute_package_default
-          $compute_service    = $compute_service_default
-          $controller_package = $controller_package_default
-          $controller_service = $controller_service_default
-          $libvirt_service    = $libvirt_service_default
-          $qemu_conf          = $qemu_conf_default
+          $compute_metadata_service = $compute_metadata_service_default
+          $compute_metadata_package = $compute_metadata_package_default
+          $compute_package          = $compute_package_default
+          $compute_service          = $compute_service_default
+          $controller_package       = $controller_package_default
+          $controller_service       = $controller_service_default
+          $libvirt_service          = $libvirt_service_default
+          $qemu_conf                = $qemu_conf_default
         }
       }
     }
     'RedHat' : {
       validate_re($::operatingsystemmajrelease, '^7$', 'A 7.x Red Hat based system is required.')
-      $compute_package    = $compute_package_default
-      $compute_service    = $compute_service_default
-      $controller_package = $controller_package_default
-      $controller_service = $controller_service_default
-      $libvirt_service    = $libvirt_service_default
-      $qemu_conf          = $qemu_conf_default
+      $compute_metadata_service = $compute_metadata_service_default
+      $compute_metadata_package = $compute_metadata_package_default
+      $compute_package          = $compute_package_default
+      $compute_service          = $compute_service_default
+      $controller_package       = $controller_package_default
+      $controller_service       = $controller_service_default
+      $libvirt_service          = $libvirt_service_default
+      $qemu_conf                = $qemu_conf_default
     }
     default: {
       validate_re($::osfamily, '^(Debian|RedHat|Archlinux)$', 'Debian or Red Hat based system required.')
