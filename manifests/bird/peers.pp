7817e8f6c1ccaa56777b1b2547895569b5a43293
Jan Ivar Beddari <code@beddari.net>
2015-09-29 11:10:13 +0200

    local_ip per protocol
    Splits local_ip into local_ipv4 and local_ipv6 to support different
    values per protocol when rendering templates.

--- manifests/bird/peers.pp
+++ manifests/bird/peers.pp
@@ -7,26 +7,26 @@ define calico::bird::peers(
   validate_hash($peer_defaults)
   validate_hash($peers)
 
-  $peer = $peers[$name]
+  $peer = deep_merge($peer_defaults, $peers[$name])
 
-  if has_key($peer, 'ipv4') {
+  if has_key($peer, 'peer_ipv4') {
     calico::bird::peer { "${name}_ipv4":
       protocol => 'ipv4',
-      peer_ip  => $peer['ipv4'],
+      peer_ip  => $peer['peer_ipv4'],
       peer_as  => $peer['peer_as'],
       local_as => $peer['local_as'],
-      local_ip => $peer['local_ip'],
+      local_ip => $peer['local_ipv4'],
       template => $peer_template,
     }
   }
 
-  if has_key($peer, 'ipv6') {
+  if has_key($peer, 'peer_ipv6') {
     calico::bird::peer { "${name}_ipv6":
       protocol => 'ipv6',
-      peer_ip  => $peer['ipv6'],
+      peer_ip  => $peer['peer_ipv6'],
       peer_as  => $peer['peer_as'],
       local_as => $peer['local_as'],
-      local_ip => $peer['local_ip'],
+      local_ip => $peer['local_ipv6'],
       template => $peer_template,
     }
   }
